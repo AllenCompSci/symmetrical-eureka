44a8015661cf19cbf2b0b2e9fad37dfe901e6a34
jharls <joshaharlan@gmail.com>
2017-02-02 14:27:37 -0600

    Add files via upload
    this is updated as of 2/2/2017

--- CompSci2Game/src/CompSci2Game/src/graphics.java
+++ CompSci2Game/src/CompSci2Game/src/graphics.java
@@ -1,33 +1,35 @@
-/**
- * Created by 226784 on 1/27/2017.
- */
+package game;
 
 import java.awt.FlowLayout;
 import javax.swing.*;
-import javax.swing.JLabel;
 
 public class graphics extends JFrame {
 
-
-
-    private JLabel item1;
-    private JLabel imageJLabel;
-
-
+	private JLabel window;
     public graphics(){
-        super("KEEEP THE BALLOON UUPPPPP");
+        super("Keep the Balloon Up! :D");
         setLayout(new FlowLayout());
-
-        item1 = new JLabel("TESTTTTTTT");
-        item1.setToolTipText("this is a major teest");
-        add(item1);
-
-
-        ImageIcon image = new ImageIcon("C:\\Users\\226784\\IdeaProjects\\CompSci2Game\\src\\head.png");
-        JLabel imageLabel = new JLabel(image);
-        imageLabel.setBounds(10,10,400,400);
+    
+        window = new JLabel("DONT LET THE BALLOON FALL!");
+        window.setToolTipText("this is a major teest");
+        ImageIcon winIcon = new ImageIcon("C:\\Users\\227385\\Pictures\\whyzay\\dogekappa10.png");
+        this.setIconImage(winIcon.getImage());
+        add(window); 
+       
+        //window.setText("Keep the Balloon Up!");
+        	
+        ImageIcon wallpaper = new ImageIcon("C:\\Users\\227385\\Pictures\\charcoal.png");
+        JLabel imageLabel = new JLabel(wallpaper);
+        imageLabel.setBounds(100,100,32,32);
         imageLabel.setVisible(true);
+        
+        ImageIcon balloon = new ImageIcon("C:\\Users\\227385\\Pictures\\whyzay\\dogekappa10.png");
+        JLabel imageLabel1 = new JLabel(balloon);
+        imageLabel1.setBounds(100,100,32,32);
+        imageLabel1.setVisible(true);
 
+
+        add(imageLabel1);
         add(imageLabel);
 
 
